537c69d615034c47e84e59d1ea3465b24fdcf610
Scott Finnie <scott.finnie@hymans.co.uk>
2021-10-29 13:28:01 +0100

    use sqlite as persistence mechanism

--- bookmarks/application.py
+++ bookmarks/application.py
@@ -2,11 +2,17 @@ from eventsourcing.application import Application
 from typing import Dict, List
 from bookmarks.domain import BookmarkCollection
 
+# Use SQLite as a persistent store of data instead of the default
+# transient, in-memory store.
+import os
+os.environ["INFRASTRUCTURE_FACTORY"] = "eventsourcing.sqlite:Factory"
+os.environ["SQLITE_DBNAME"] = "data/bookmarks.db"
+
 class BookmarksApplication(Application):
 
     def __init__(self):
         super().__init__()
-        self.bookmarks: BookmarkCollection = BookmarkCollection("default_collection")
+        self.bookmarks: BookmarkCollection = BookmarkCollection()
     
     def add_bookmark(self, name: str, url: str):
         self.bookmarks.add_bookmark(name, url)
